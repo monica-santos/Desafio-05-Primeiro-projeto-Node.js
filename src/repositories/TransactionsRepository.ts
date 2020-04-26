05a51e02ac0e1b4989b68355d8024ad986d7d875
Mônica Santos <monicasantosdev@gmail.com>
2020-04-26 16:55:21 -0300

    implement transactions challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,32 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return [...this.transactions];
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (acc, cur) => {
+        if (cur.type === 'income') {
+          acc.income += cur.value;
+        } else {
+          acc.outcome += cur.value;
+        }
+        acc.total = acc.income - acc.outcome;
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = { id: uuid(), title, value, type };
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
